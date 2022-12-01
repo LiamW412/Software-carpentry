1ab69b386f6b981aeed3e5d4b469efcf48b1dadd
LiamW412 <liam.walker6@nhs.net>
2022-12-01 22:17:17 +0000

    More tests added...

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import numpy as np
 import numpy.testing as npt
-from inflammation.models import daily_max
+import random
+# from inflammation.models import daily_max
 
 
-data = np.loadtxt(fname = 'data/inflammation-01.csv', delimiter=',')
+# data = np.loadtxt(fname = 'data/inflammation-01.csv', delimiter=',')
 
-daily_max([[1,2], [3,4], [5,6]])
+# daily_max([[1,2], [3,4], [5,6]])
 
 # test_input_pass = np.array([[1,2], [3,4], [5,6]])
 # test_input_fail = np.array([[2,0], [3,4], [5,6]])
 # test_result = np.array([3,4])
 
 # npt.assert_array_equal(daily_mean(test_input_fail), test_result)
+
+random.seed(1)
+print(random.sample(range(0,100), 10))
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -38,3 +39,14 @@ def test_daily_max():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0,0],[0,0],[0,0]], [0,0]),
+        ([[1,2],[3,4],[5,6]], [3,4])
+    ]
+)
+def test_daily_mean(test, expected):
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
